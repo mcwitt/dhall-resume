c112aea41d473ae1bfca11d384459a90dacbc977
Matt Wittmann <mcwitt@gmail.com>
2020-04-20 17:20:13 -0700

    Update nix build

--- dhall-resume.nix
+++ dhall-resume.nix
@@ -1,5 +1,5 @@
-{ mkDerivation, base, dhall, HaTeX, hspec, pandoc, raw-strings-qq
-, stdenv, text
+{ mkDerivation, base, dhall, HaTeX, hspec, optparse-applicative
+, pandoc, raw-strings-qq, stdenv, text
 }:
 mkDerivation {
   pname = "dhall-resume";
@@ -8,7 +8,7 @@ mkDerivation {
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [ base dhall HaTeX pandoc text ];
-  executableHaskellDepends = [ base ];
+  executableHaskellDepends = [ base optparse-applicative text ];
   testHaskellDepends = [ base hspec raw-strings-qq text ];
   license = stdenv.lib.licenses.mit;
 }
